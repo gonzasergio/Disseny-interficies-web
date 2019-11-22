4d17c51410d6e53b1a5367d514118dd5fa794fa7
Sergio <sgv721@gmail.com>
2019-11-22 13:17:23 +0100

    Començament

---
+++ Projecte/html/home.php
@@ -0,0 +1,9 @@
+<html>
+<?php include 'head.html'?>
+<body>
+<?php include 'navbar.html'?>
+<main>
+</main>
+<?php include 'footer.html'?>
+</body>
+</html>
---
+++ Projecte/html/ofertas.php
@@ -0,0 +1,7 @@
+<html>
+<?php include 'head.html'?>
+<body>
+<?php include 'navbar.html'?>
+<?php include 'footer.html'?>
+</body>
+</html>
