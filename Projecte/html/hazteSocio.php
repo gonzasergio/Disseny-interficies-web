5f12de0695fbfe761b1ece9a286e45352a98fa17
Sergio <sgv721@gmail.com>
2020-02-05 08:59:47 +0100

    CARROUSEL, PISTAS & FORMS

--- Projecte/html/hazteSocio.php
+++
@@ -1,7 +0,0 @@
-<html>
-<?php include 'head.html'?>
-<body>
-<?php include 'navbar.html'?>
-<?php include 'footer.html'?>
-</body>
-</html>
--- Projecte/html/home.php
+++
@@ -1,9 +0,0 @@
-<html>
-<?php include 'head.html'?>
-<body>
-<?php include 'navbar.html'?>
-<main>
-</main>
-<?php include 'footer.html'?>
-</body>
-</html>
